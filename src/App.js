724f95d422552e56a331a0a739d4a7f72ad7e586
Guilherme Nunes <guiicnunes@gmail.com>
2021-06-07 15:52:53 -0300

    Resolucao exec

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StrictAccess from './StrictAccess';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" component={ Home } />
-        <Route path="/about" component={ About } />
-        <Route path="/users" component={ Users } />
+        <Switch>
+          <Route path="/users/:id" render={ (props) => <Users greetingsMessage="Good Morning" { ...props } /> } />
+          <Route path="/about" component={ About } />
+          <Route path="/strict-access" render={ () => <StrictAccess user={ {username: 'joao', password: 1234}} /> } />
+          <Route path="/" component={ Home } />
+        </Switch>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/Users">Users</Link></li>
+          <li><Link to="/strict-access">Strict Access</Link></li>
         </ul>
       </BrowserRouter>
     );
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,14 +1,18 @@
+// arquivo Users.js
 import React, { Component } from 'react';
 
 class Users extends Component {
   render() {
+    const { greetingsMessage, match } = this.props;
+    const { id } = match.params;
     return (
       <div>
         <h2>Users</h2>
-        <p> My awesome Users component </p>
+        <p> { greetingsMessage }, My awesome Users({ id }) component </p>
       </div>
     );
   }
 };
 
 export default Users;
+
